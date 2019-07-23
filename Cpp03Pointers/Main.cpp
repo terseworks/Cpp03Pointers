3c43b5a1f90f8efea9ace20bb52b240b6d639cbd
John Meschke <jmprogramming2dx@gmail.com>
2019-07-22 20:08:13 -0500

    Add project files.

---
+++ Cpp03Pointers/Main.cpp
@@ -0,0 +1,98 @@
+//********************************************************************************
+// Main.cpp
+//
+// Written by: John Meschke
+// Description: Main program entry point.
+//********************************************************************************
+
+#include "Pointers.h"
+#include <iostream>
+
+//********************************************************************************
+// Item
+//********************************************************************************
+
+class Item
+{
+	public:
+		Item();
+		Item(int id);
+		~Item();
+		int getId() const;
+
+	private:
+		int id;
+};
+
+/*
+Item constructor.
+*/
+Item::Item() : Item(0)
+{
+}
+
+/*
+Item constructor.
+*/
+Item::Item(int id) : id(id)
+{
+	std::cout << "Constructing item " << id << "." << std::endl;
+}
+
+/*
+Item destructor.
+*/
+Item::~Item()
+{
+	std::cout << "Destructing item " << id << "." << std::endl;
+}
+
+/*
+Get the item's ID.
+*/
+int Item::getId() const
+{
+	return id;
+}
+
+//********************************************************************************
+// Main
+//********************************************************************************
+
+/*
+Main entry function.
+*/
+int main()
+{
+	// Scoped pointer test 1.
+	{
+		ScopedPointer<Item> singleItem = new Item(100);
+		ScopedArray<Item> multiItem = new Item[3];
+	}
+	std::cout << std::endl;
+
+	// Scoped pointer test 2.
+	{
+		ScopedPointer<Item> resetItem = new Item(200);
+		resetItem.reset(new Item(300));
+	}
+	std::cout << std::endl;
+
+	// Scoped pointer test 3.
+	{
+		ScopedPointer<Item> swapItem1 = new Item(400);
+		ScopedPointer<Item> swapItem2 = new Item(500);
+		std::cout << "swapItem1: " << swapItem1->getId() << std::endl;
+		std::cout << "swapItem2: " << swapItem2->getId() << std::endl;
+		swapItem1.swap(swapItem2);
+		std::cout << "Items swapped." << std::endl;
+		std::cout << "swapItem1: " << swapItem1->getId() << std::endl;
+		std::cout << "swapItem2: " << swapItem2->getId() << std::endl;
+	}
+	std::cout << std::endl;
+
+	std::cout << "Press <enter> to quit.";
+	std::cin.get();
+
+	return 0;
+}
---
+++ Cpp03Pointers/Pointers.h
@@ -0,0 +1,233 @@
+//********************************************************************************
+// Pointers.h
+//
+// Written by: John Meschke
+// Description: Smart pointer implementation.
+//********************************************************************************
+
+#ifndef INCLUDE_POINTERS
+#define INCLUDE_POINTERS
+
+#include <cstdlib>
+
+//********************************************************************************
+// ScopedPointer
+//********************************************************************************
+
+template<typename typeA>
+class ScopedPointer
+{
+	public:
+		ScopedPointer();
+		ScopedPointer(typeA *newMemory);
+		~ScopedPointer();
+		typeA &operator*() const;
+		typeA *operator->() const;
+		typeA *get() const;
+		typeA *release();
+		void reset(typeA *newMemory);
+		void swap(ScopedPointer<typeA> &scopedPointer);
+
+	private:
+		typeA *memory;
+
+		ScopedPointer(const ScopedPointer &scopedPointer);
+		ScopedPointer &operator=(const ScopedPointer &rhs);
+};
+
+/*
+ScopedPointer constructor.
+*/
+template<typename typeA>
+ScopedPointer<typeA>::ScopedPointer() : memory(0)
+{
+}
+
+/*
+ScopedPointer constructor.
+*/
+template<typename typeA>
+ScopedPointer<typeA>::ScopedPointer(typeA *newMemory) : memory(newMemory)
+{
+}
+
+/*
+ScopedPointer destructor.
+*/
+template<typename typeA>
+ScopedPointer<typeA>::~ScopedPointer()
+{
+	delete memory;
+}
+
+/*
+ScopedPointer * operator.
+*/
+template<typename typeA>
+typeA &ScopedPointer<typeA>::operator*() const
+{
+	return *memory;
+}
+
+/*
+ScopedPointer -> operator.
+*/
+template<typename typeA>
+typeA *ScopedPointer<typeA>::operator->() const
+{
+	return memory;
+}
+
+/*
+Return a raw pointer to the memory.
+*/
+template<typename typeA>
+typeA *ScopedPointer<typeA>::get() const
+{
+	return memory;
+}
+
+/*
+Releases ownership of the memory to a raw pointer.
+Object no longer is responsible for memory deletion.
+*/
+template<typename typeA>
+typeA *ScopedPointer<typeA>::release()
+{
+	typeA *temp = memory;
+	memory = 0;
+
+	return temp;
+}
+
+/*
+Clear the existing memory and reassign to new memory.
+Can also be used to nullify a pointer.
+*/
+template<typename typeA>
+void ScopedPointer<typeA>::reset(typeA *newMemory)
+{
+	if (memory != newMemory)
+	{
+		delete memory;
+		memory = newMemory;
+	}
+}
+
+/*
+Swap the contents and ownership of the memory.
+*/
+template<typename typeA>
+void ScopedPointer<typeA>::swap(ScopedPointer<typeA> &scopedPointer)
+{
+	typeA *temp = memory;
+	memory = scopedPointer.memory;
+	scopedPointer.memory = temp;
+}
+
+//********************************************************************************
+// ScopedArray
+//********************************************************************************
+
+template<typename typeA>
+class ScopedArray
+{
+	public:
+		ScopedArray();
+		ScopedArray(typeA *newMemory);
+		~ScopedArray();
+		typeA &operator[](size_t index) const;
+		typeA *get() const;
+		typeA *release();
+		void reset(typeA *newMemory);
+		void swap(ScopedArray<typeA> &scopedArray);
+
+	private:
+		typeA *memory;
+
+		ScopedArray(const ScopedArray &scopedArray);
+		ScopedArray &operator=(const ScopedArray &rhs);
+};
+
+/*
+ScopedArray constructor.
+*/
+template<typename typeA>
+ScopedArray<typeA>::ScopedArray() : memory(0)
+{
+}
+
+/*
+ScopedArray constructor.
+*/
+template<typename typeA>
+ScopedArray<typeA>::ScopedArray(typeA *newMemory) : memory(newMemory)
+{
+}
+
+/*
+ScopedArray destructor.
+*/
+template<typename typeA>
+ScopedArray<typeA>::~ScopedArray()
+{
+	delete[] memory;
+}
+
+/*
+ScopedArray [] operator.
+*/
+template<typename typeA>
+typeA &ScopedArray<typeA>::operator[](size_t index) const
+{
+	return memory[index];
+}
+
+/*
+Return a raw pointer to the memory.
+*/
+template<typename typeA>
+typeA *ScopedArray<typeA>::get() const
+{
+	return memory;
+}
+
+/*
+Releases ownership of the memory to a raw pointer.
+Object no longer is responsible for memory deletion.
+*/
+template<typename typeA>
+typeA *ScopedArray<typeA>::release()
+{
+	typeA *temp = memory;
+	memory = 0;
+
+	return temp;
+}
+
+/*
+Clear the existing memory and reassign to new memory.
+Can also be used to nullify a pointer.
+*/
+template<typename typeA>
+void ScopedArray<typeA>::reset(typeA *newMemory)
+{
+	if (memory != newMemory)
+	{
+		delete[] memory;
+		memory = newMemory;
+	}
+}
+
+/*
+Swap the contents and ownership of the memory.
+*/
+template<typename typeA>
+void ScopedArray<typeA>::swap(ScopedArray<typeA> &scopedArray)
+{
+	typeA *temp = memory;
+	memory = scopedArray.memory;
+	scopedArray.memory = temp;
+}
+
+#endif
